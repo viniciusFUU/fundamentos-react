5be737c589b28c1536e2171fe6840bfaff8e3050
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-09-08 14:38:42 -0300

    class 11 component

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 export default (props) => {
+  const min = 50;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * (max - min)) + min;
+  const gerarVerdadeiro = () => Math.random() > 0.5;
   return (
     <div>
       <div>Filho</div>
-      <button onClick={
-         (e) => {
-            props.quandoClicar ('João', 53, true)
-      }}>
+      <button
+        onClick={(e) => {
+          props.quandoClicar("João", gerarIdade(), gerarVerdadeiro());
+        }}
+      >
         Fornercer informações
       </button>
     </div>
